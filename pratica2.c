c2d2c4bc1622d91c83ac9872e2eef4ce53c9dca5
Borromeu Jr <borromeujunior@hotmail.com>
2025-11-23 04:57:25 +0000

    ok

--- pratica2.c
+++ pratica2.c
@@ -1,14 +1,15 @@
-int main(){
+#include <stdio.h>
 
+    int main(){
+        int vetor[5] = {1,2,3,4,5};
+        int soma = 0; // Variável criada para guardar a soma do vetor
 
+        // Calculando a soma dos elementos do vetor
+        for (int i = 0; i < 5; i++){
+            soma += vetor[i];
+        }
 
-
-
-
-
-
-
-
+        printf("A soma dos elementos do vetor é: %d\n", soma);
 
 
     return 0;
