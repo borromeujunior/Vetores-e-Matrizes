4506c3424e325486466aa51fd3230c6ef8f2c0ea
Borromeu Jr <borromeujunior@hotmail.com>
2025-11-24 03:05:34 +0000

    pratica5

---
+++ pratica5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define LINHA 5
+#define COLUNA 5
+
+int main(){
+    int matriz[LINHA][COLUNA]; 
+    int target = 9;
+    int found = 0;
+    int soma = 1;
+
+    for (int i = 0; i < LINHA; i++) {      
+        for (int j = 0; j < COLUNA; j++) {
+            matriz [i][j] = soma;
+            soma++;
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Busca condicional do elemento alvo
+    for (int i = 0; i < LINHA; i++) {      // Loop externo para as linhas
+        for (int j = 0; j < COLUNA; j++) {  // Loop interno para as colunas
+            if (matriz[i][j] == target) {
+                printf("O valor %d foi encontrado no indice (%d, %d)\n", target, i, j);
+                found = 1;
+                break;
+            }
+        }
+        if (found) break;
+    }
+ 
+    if (!found) {
+        printf("Elemento %d não encontrado na matriz\n", target);
+    }
+
+    return 0;
+}
